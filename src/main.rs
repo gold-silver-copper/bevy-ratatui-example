1d9c346b9b4b63b570af58151b65da073c2920c2
zombkit <bakhmetstephen@gmail.com>
2024-02-18 14:54:11 -0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -13,16 +13,9 @@
 //! [examples]: https://github.com/ratatui-org/ratatui/blob/main/examples
 //! [examples readme]: https://github.com/ratatui-org/ratatui/blob/main/examples/README.md
 
-use crossterm::{
-    event::{self, KeyCode, KeyEventKind},
-    terminal::{
-        disable_raw_mode, enable_raw_mode, EnterAlternateScreen,
-        LeaveAlternateScreen,
-    },
-    ExecutableCommand,
-};
+use bevy::prelude::*;
 use ratatui::{
-    prelude::{CrosstermBackend, Stylize, Terminal},
+    prelude::{BevyBackend, Stylize, Terminal},
     widgets::Paragraph,
 };
 use std::io::{stdout, Result};
@@ -36,9 +29,7 @@ use std::io::{stdout, Result};
 /// events or update the application state. It just draws a greeting and exits when the user
 /// presses 'q'.
 fn main() -> Result<()> {
-    stdout().execute(EnterAlternateScreen)?;
-    enable_raw_mode()?;
-    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+    let mut terminal = Terminal::new(BevyBackend::new(5))?;
     terminal.clear()?;
 
     loop {
@@ -51,18 +42,11 @@ fn main() -> Result<()> {
                 area,
             );
         })?;
-        if event::poll(std::time::Duration::from_millis(16))? {
-            if let event::Event::Key(key) = event::read()? {
-                if key.kind == KeyEventKind::Press
-                    && key.code == KeyCode::Char('q')
-                {
-                    break;
-                }
-            }
+        if true {
+           todo!();
         }
     }
 
-    stdout().execute(LeaveAlternateScreen)?;
-    disable_raw_mode()?;
+
     Ok(())
 }
